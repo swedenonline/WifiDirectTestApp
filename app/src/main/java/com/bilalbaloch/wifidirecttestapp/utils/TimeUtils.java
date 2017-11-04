4758157aa82f6d816811e48c004dd77935e25499
Fateh Muhammad Bilal <fatehmuhammadbilal@gmail.com>
2017-11-04 14:24:44 +0100

    Fix date format in PingView (#1)

--- app/src/main/java/com/bilalbaloch/wifidirecttestapp/utils/TimeUtils.java
+++ app/src/main/java/com/bilalbaloch/wifidirecttestapp/utils/TimeUtils.java
@@ -1,7 +1,9 @@
 package com.bilalbaloch.wifidirecttestapp.utils;
 
+import android.util.Log;
+
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by bilal on 2017-11-02.
@@ -9,8 +11,9 @@ import java.util.Calendar;
 
 public final class TimeUtils {
 
-    private static final String DATE_FORMAT = "hh:mm:ss";
+    private static final String DATE_FORMAT = "HH:mm:ss:SSS";
     private static final SimpleDateFormat FORMATTER = new SimpleDateFormat(DATE_FORMAT);
+    private static final String TAG = TimeUtils.class.getSimpleName();
 
     /**
      * Constructor.
@@ -23,8 +26,8 @@ public final class TimeUtils {
      * Returns current time in milliseconds.
      */
     public static String getCurrentTime() {
-        final Calendar c = Calendar.getInstance();
-        return String.valueOf(c.get(Calendar.MILLISECOND));
+        Log.d(TAG, FORMATTER.format(new Date()));
+        return FORMATTER.format(new Date());
     }
 
     /**
@@ -33,9 +36,7 @@ public final class TimeUtils {
      * @param timeInMillis milliseconds to convert.
      */
     public static String getHumanReadableTimeFormat(final String timeInMillis) {
-        final Long millis = Long.parseLong(timeInMillis);
-        final Calendar c = Calendar.getInstance();
-        c.setTimeInMillis(millis);
-        return FORMATTER.format(c.getTime());
+        Log.d(TAG, timeInMillis);
+        return timeInMillis;
     }
 }
